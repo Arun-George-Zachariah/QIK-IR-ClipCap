70712303b5637fe47153db1acb5cff9b7a10cf54
Arun George Zachariah <az2z7@mail.umkc.edu>
2022-06-04 16:08:00 -0500

    Adding pre-constructed features for CroW evaluation

--- QIK_Evaluation/create_frcnn_results.py
+++ QIK_Evaluation/create_frcnn_results.py
@@ -7,7 +7,7 @@ import logging
 import datetime
 import argparse
 import pickle
-import frcnn_search
+import fr_cnn_search
 
 # Local constants
 EVAL_K = 16
@@ -21,43 +21,43 @@ def retrieve(query_image):
     # Get CroW results
     time = datetime.datetime.now()
 
-    frcnn_results = []
+    fr_cnn_results = []
 
     # Fetching the candidates from CroW.
-    frcnn_pre_results = frcnn_search.frcnn_search(query_image_path, EVAL_K + 1)
+    fr_cnn_pre_results = fr_cnn_search.fr_cnn_search(query_image_path, EVAL_K + 1)
 
     # Noting CroW time.
-    frcnn_time = datetime.datetime.now() - time
-    print("create_frcnn_results.py :: CroW Fetch Execution time :: ", frcnn_time)
-    logging.info("create_frcnn_results.py :: CroW Fetch Execution time :: %s", str(frcnn_time))
+    fr_cnn_time = datetime.datetime.now() - time
+    print("create_fr_cnn_results.py :: CroW Fetch Execution time :: ", fr_cnn_time)
+    logging.info("create_fr_cnn_results.py :: CroW Fetch Execution time :: %s", str(fr_cnn_time))
 
     # Removing query image from the result set.
-    for res in frcnn_pre_results:
+    for res in fr_cnn_pre_results:
         img_file = res.rstrip().split("/")[-1]
         if img_file == query_image:
             continue
-        frcnn_results.append(res.rstrip().split("/")[-1])
-    print("create_frcnn_results.py :: CroW :: frcnn_results :: ", frcnn_results)
+        fr_cnn_results.append(res.rstrip().split("/")[-1])
+    print("create_fr_cnn_results.py :: CroW :: fr_cnn_results :: ", fr_cnn_results)
 
     # Adding data to the return dictionary.
-    ret_dict["frcnn_time"] = frcnn_time.total_seconds()
-    ret_dict["frcnn_results"] = frcnn_results
+    ret_dict["fr_cnn_time"] = fr_cnn_time.total_seconds()
+    ret_dict["fr_cnn_results"] = fr_cnn_results
 
     # Writing the output to a file.
-    with open("data/CroW_Results_Dict.txt", 'a+') as f:
+    with open("data/FR_CNN_Results_Dict.txt", 'a+') as f:
         f.write(query_image + ":: " + str(ret_dict) + "\n")
 
-    print("create_frcnn_results.py :: retrieve :: ret_dict :: ", str(ret_dict))
+    print("create_fr_cnn_results.py :: retrieve :: ret_dict :: ", str(ret_dict))
     return ret_dict
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess to extract text from a folder of csv files.')
     parser.add_argument('-data', default="data/15K_Dataset.pkl", metavar='data', help='Pickled file containing the list of images.', required=False)
-    parser.add_argument('-out', default="data/CroW_Results.pkl", metavar='data', help='Directory to write the CroW Results.', required=False)
+    parser.add_argument('-out', default="data/FR_CNN_Results.pkl", metavar='data', help='Directory to write the FR-CNN Results.', required=False)
     args = parser.parse_args()
 
     # Initializing CroW.
-    frcnn_search.init()
+    fr_cnn_search.init()
 
     # Initializing a dictionary to hold the results.
     results_dict = {}
--- QIK_Evaluation/setup_scripts/setup_frcnn.sh
+++ QIK_Evaluation/setup_scripts/setup_frcnn.sh
@@ -16,8 +16,10 @@ pip install opencv-python==4.2.0.32
 # Setting python path after installation.
 export PYTHONPATH=$QIK_HOME/ML_Models/DeepVision/py-faster-rcnn/caffe-fast-rcnn/python:$PYTHONPATH
 
-# Extracting features.
-python read_data.py && python features.py
+# Extracting features. (Note features needs to be extraced with a GPU)
+# python read_data.py && python features.py
+wget https://mailmissouri-my.sharepoint.com/:u:/g/personal/az2z7_umsystem_edu/EfA4JX6_9CNGpUJo9EombI4Bc47B8o2aqt153Xejprqjuw?download=1 -O $QIK_HOME/ML_Models/DeepVision/data
+cd $QIK_HOME/ML_Models/DeepVision/data && tar -xvf FR_CNN_Features.tar
 
 # Starting the FR-CNN web app.
 python qik_search.py &>> /dev/null &
